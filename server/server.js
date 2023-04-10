29eb3a4f0020c71d9260b645f758d74d30345f44
leerivera <leerivera80@gmail.com>
2023-04-10 13:46:58 -0400

    user routes

--- server/server.js
+++ server/server.js
@@ -2,14 +2,17 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const usersRouter = require('./routes/users');
+
 
 dotenv.config();
+const uri = process.env.MONGODB_URI;
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use('/users', usersRouter);
 
-const uri = process.env.MONGODB_URI;
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true });
 
 const connection = mongoose.connection;
