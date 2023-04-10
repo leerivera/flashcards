df8f2fadbff475eac6fbdaad71def6ccf9517b10
leerivera <leerivera80@gmail.com>
2023-04-10 14:37:44 -0400

    dbconnect

--- server/server.js
+++ server/server.js
@@ -1,27 +1,27 @@
 const express = require('express');
-const cors = require('cors');
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
-const usersRouter = require('./routes/users');
-
-
-dotenv.config();
-const uri = process.env.MONGODB_URI;
-
 const app = express();
+const cors = require('cors');
+
+const connectDB = require("./config/database");
+
+const usersRouter = require('./routes/users');
+//Use .env file in config folder
+require("dotenv").config({ path: "./config/.env" });
+
+// Passport config
+// require("./config/passport")(passport);
+
+//Connect To Database
+connectDB();
+
+
 app.use(cors());
 app.use(express.json());
 app.use('/users', usersRouter);
 
-mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true });
 
-const connection = mongoose.connection;
-connection.once('open', () => {
-  console.log('MongoDB database connection established successfully');
-});
 
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port: ${PORT}`);
+//Server Running
+app.listen(process.env.PORT, () => {
+  console.log("Server is running, you better catch it!");
 });
