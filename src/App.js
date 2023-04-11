26fa6a1a9f8b2f27376d95475331a488011941e7
leerivera <leerivera80@gmail.com>
2023-04-11 15:01:38 -0400

    app.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Switch, Link } from 'react-router-dom';
+import Register from './components/Register';
+import Login from './components/Login';
+import FlashcardManager from './components/FlashcardManager';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav>
+        <Link to="/register">Register</Link>
+        <Link to="/login">Login</Link>
+        <Link to="/cards">Flashcard Manager</Link>
+      </nav>
+
+      <Switch>
+        <Route path="/register" component={Register} />
+        <Route path="/login" component={Login} />
+        <Route path="/cards" component={FlashcardManager} />
+      </Switch>
     </div>
   );
 }
 
 export default App;
+
