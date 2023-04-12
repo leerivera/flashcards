c1343406fba87c3dcc4802e24909938dc3f09fa3
leerivera <leerivera80@gmail.com>
2023-04-12 13:41:05 -0400

    login

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import Register from './components/Register';
 import Login from './components/Login';
 import FlashcardManager from './components/FlashcardManager';
 import CreateFlashcard from './components/CreateFlashcard';
+import Home from './components/Home';
 
 function App() {
   return (
     <div className="App">
       <nav>
+        <Link to="/">Home</Link>
         <Link to="/register">Register</Link>
         <Link to="/login">Login</Link>
         <Link to="/cards">Flashcard Manager</Link>
@@ -15,6 +17,7 @@ function App() {
       </nav>
 
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
         <Route path="/cards" element={<FlashcardManager />} />
---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Welcome to the Flashcard App!</h1>
+      <p>Start by registering, logging in, or managing your flashcards.</p>
+    </div>
+  );
+};
+
+export default Home;
